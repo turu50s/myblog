90d7f0046fc9ebea0e58ff862cff3311c0b2e0cb
turuki <sigeru0216@gmail.com>
2018-08-17 11:11:43 +0900

    Add link

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 5.times do |i|
-  Post.create(title: "title #{i}", body: "#{i}")
+  Post.create(title: "title #{i}", body: "body #{i}")
 end
